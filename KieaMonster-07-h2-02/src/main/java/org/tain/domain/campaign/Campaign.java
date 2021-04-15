49c14d772cce11f198a0c2ab193e3beb584cacac
pch8349 <chan449@naver.com>
2021-04-15 15:06:00 +0900

    h2

---
+++ KieaMonster-07-h2-02/src/main/java/org/tain/domain/campaign/Campaign.java
@@ -0,0 +1,43 @@
+package org.tain.domain.campaign;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.tain.utils.LocalDateDeserializer;
+import org.tain.utils.LocalDateSerializer;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "ci_campaign")
+@Data
+public class Campaign {
+
+	@Id
+	@Column(name = "campaign_sk")
+	private Long campaignSk;
+	
+	@Column(name = "campaign_cd", length = 256)
+	private String campaignCd;
+	
+	@Column(name = "campaign_nm", length = 256)
+	private String campaignNm;
+	
+	@Column(name = "campaign_folder_txt", length = 256)
+	private String campaignFolderTxt;
+	
+
+	@Column(name = "processed_dttm")
+	@JsonSerialize(using = LocalDateSerializer.class)
+	@JsonDeserialize(using = LocalDateDeserializer.class)
+	@CreationTimestamp
+	private LocalDate processedDttm;
+}
